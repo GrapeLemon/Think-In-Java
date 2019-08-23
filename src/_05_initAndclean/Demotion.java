ca3d97c2bf669256a5d3d2cea4d70055ae6a92d4
lwx <279214678@qq.com>
2019-08-23 20:58:43 +0800

    涉及基本类型的重载，会有一个提升和降级的效果

---
+++ src/_05_initAndclean/Demotion.java
@@ -0,0 +1,8 @@
+package _05_initAndclean;
+
+/**
+ * @author lwx
+ * @date 2019/8/23-20:40
+ */
+public class Demotion {
+}
---
+++ src/_05_initAndclean/PrimitiveOverloading.java
@@ -0,0 +1,8 @@
+package _05_initAndclean;
+
+/**
+ * @author lwx
+ * @date 2019/8/23-20:10
+ */
+public class PrimitiveOverloading {
+}
