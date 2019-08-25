0231d931b84a7c28986e37f17f0fb3a6e84d709b
lwx <279214678@qq.com>
2019-08-25 22:42:14 +0800

    this用法两种,一种表示当前对象一种是在构造器里面调用一个其他的构造器

---
+++ src/_05_initAndclean/_4_this/Flower.java
@@ -0,0 +1,8 @@
+package _05_initAndclean._4_this;
+
+/**
+ * @author lwx
+ * @date 2019/8/23-21:47
+ */
+public class Flower {
+}
---
+++ src/_05_initAndclean/pratice/A.java
@@ -0,0 +1,8 @@
+package _05_initAndclean.pratice;
+
+/**
+ * @author lwx
+ * @date 2019/8/23-21:42
+ */
+public class A {
+}
