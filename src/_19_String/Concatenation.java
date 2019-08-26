9cef13279131c5e2e733822ce92bb91977a8f630
lwx <279214678@qq.com>
2019-08-26 13:34:49 +0800

    杂项..

--- src/_19_String/Concatenation.java
+++ src/_19_String/Concatenation.java
@@ -1,9 +1,12 @@
 package _19_String;
 
+import static util.Print.*;
+
 public class Concatenation {
     public static void main(String[] args) {
         String mango = "mango";
         String s = "abc" + mango + "def" + 47;
-        System.out.println(s);
+        print();
+        print(123);
     }
 }
