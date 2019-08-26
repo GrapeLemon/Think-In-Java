d415bc501dd01b1fa4e9f0ec3ecee06591651c1b
lwx <279214678@qq.com>
2019-08-26 13:33:54 +0800

    把IDEA的导包优化干掉会方便一点

--- src/util/Print.java
+++ src/util/Print.java
@@ -5,7 +5,12 @@ package util;
  * @Date 2019/8/23 10:38
  **/
 public class Print {
-    public static void print(String s){
+
+    public static void print(){
+        System.out.println();
+    }
+
+    public static void print(Object s){
         System.out.println(s);
     }
 
---
+++ src/util/Range.java
@@ -0,0 +1,10 @@
+package util;
+
+/**
+ * @Author wx-li
+ * @Date 2019/8/26 11:08
+ *
+ **/
+public class Range {
+
+}
