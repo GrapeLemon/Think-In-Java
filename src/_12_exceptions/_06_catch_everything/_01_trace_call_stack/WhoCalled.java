8ce1bdc1cb5ea3e5a257062fb4cae5db89ae0f0a
lwx <279214678@qq.com>
2019-08-29 17:59:03 +0800

    打印栈轨迹，这个很灵魂，是我们追踪异常的核心

--- src/_12_exceptions/_06_catch_everything/_01_trace_call_stack/WhoCalled.java
+++ src/_12_exceptions/_06_catch_everything/_01_trace_call_stack/WhoCalled.java
@@ -3,6 +3,27 @@ package _12_exceptions._06_catch_everything._01_trace_call_stack;
 /**
  * @Author wx-li
  * @Date 2019/8/29 17:54
+ * Programmatic access to stack trace information.
  **/
 public class WhoCalled {
+    static void f() {
+        //Generate an exception to fill in the stack trace
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+            for (StackTraceElement ste : e.getStackTrace()) {
+                System.out.println(ste);
+            }
+        }
+    }
+    static void g(){f();}
+    static void h(){g();}
+
+    public static void main(String[] args) {
+        f();
+        System.out.println("---------------------------------------");
+        g();
+        System.out.println("---------------------------------------");
+        h();
+    }
 }
