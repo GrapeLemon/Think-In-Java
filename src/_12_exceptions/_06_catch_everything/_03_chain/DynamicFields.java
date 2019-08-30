82fe73d7eaa60ddab0d798dfcda08d811342353b
lwx <279214678@qq.com>
2019-08-30 09:19:53 +0800

    这个有点难了 看不懂他在玩什么 异常处理这么复杂的吗

--- src/_12_exceptions/_06_catch_everything/_03_chain/DynamicFields.java
+++ src/_12_exceptions/_06_catch_everything/_03_chain/DynamicFields.java
@@ -9,9 +9,7 @@ import java.io.File;
  * Demonstrates exception chaining
  **/
 
-class DynamicFieldsException extends Exception {
-
-}
+class DynamicFieldsException extends Exception {}
 
 public class DynamicFields {
     private Object[][] fields;
@@ -19,7 +17,7 @@ public class DynamicFields {
     public DynamicFields(int initialSize) {
         fields = new Object[initialSize][2];
         for (int i = 0; i < initialSize; i++) {
-            fields[i] = new Object[]{null,null};
+            fields[i] = new Object[]{null, null};
         }
     }
     public String toString() {
@@ -46,5 +44,72 @@ public class DynamicFields {
             throw new NoSuchFieldException();
         }
         return fieldNum;
+    }
+
+    private int makeField(String id) {
+        for (int i = 0; i < fields.length; i++) {
+            if (fields[i][0] == null) {
+                fields[i][0] = id;
+                return i;
+            }
+        }
+        //No empty fields.Add one:
+        Object[][] tmp = new Object[fields.length + 1][2];
+        for (int i = 0; i < fields.length; i++) {
+            tmp[i] = fields[i];
+        }
+        for (int i = fields.length; i < tmp.length; i++) {
+            tmp[i] = new Object[]{null, null};
+        }
+        fields = tmp;
+        //Recursive call with expanded fields:
+        return makeField(id);
+    }
+    public Object getField(String id) throws NoSuchFieldException {
+        return fields[getFieldNumber(id)][1];
+    }
+
+    public Object setField(String id, Object value) throws DynamicFieldsException {
+        if (value == null) {
+            //Most exceptions don't have a "cause" constructor.
+            //in these cases you must use initCause()
+            //available in all Throwable subclasses
+            DynamicFieldsException dfe = new DynamicFieldsException();
+            dfe.initCause(new NullPointerException());
+            throw dfe;
+        }
+        int fieldNumber = hasField(id);
+        if (fieldNumber == -1) {
+            fieldNumber = makeField(id);
+        }
+        Object result = null;
+        try {
+            result = getField(id);  //Get old value
+        } catch (NoSuchFieldException e) {
+            //Use constructor that takes "cause"
+            throw new RuntimeException(e);
+        }
+        fields[fieldNumber][1] = value;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        DynamicFields df = new DynamicFields(3);
+        System.out.println(df);
+        try {
+            df.setField("d", "A value for d");
+            df.setField("number", 47);
+            df.setField("number2", 48);
+            System.out.println(df);
+            df.setField("d", "A new value for d");
+            df.setField("number3", 11);
+            System.out.println("df: " + df);
+            System.out.println("df.getField(\"d\"): " + df.getField("d"));
+            Object field = df.setField("d", null);  //Exception
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace(System.out);
+        } catch (DynamicFieldsException e) {
+            e.printStackTrace(System.out);
+        }
     }
 }
