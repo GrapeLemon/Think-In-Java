9833ec07ec9b723eb0f0db52eca1274e61471cc5
lwx <279214678@qq.com>
2019-08-31 13:44:34 +0800

    门面模式:感觉挺好玩的

---
+++ src/_14_type_info/pets/Pets.java
@@ -0,0 +1,23 @@
+package _14_type_info.pets;
+
+import java.util.ArrayList;
+
+/**
+ * @Author wx-li
+ * @Date 2019/8/31 13:39
+ * Facade to produce a default PetCreator.
+ **/
+public class Pets {
+    //如果想换门面的话在把实现类换掉就好了
+    public static final PetCreator creator = new LiteralPetCreator();
+    public static Pet randomPet() {
+        return creator.randomPet();
+    }
+
+    public static Pet[] createArray(int size) {
+        return creator.createArray(size);
+    }
+    public static ArrayList<Pet> arrayList(int size) {
+        return creator.arrayList(size);
+    }
+}
