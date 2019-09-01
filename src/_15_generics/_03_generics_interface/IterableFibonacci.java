bd8b8284a9b363fd30f64e96665efc23d4488d8e
lwx <279214678@qq.com>
2019-09-01 15:34:43 +0800

    有点用继承实现代理的味道

---
+++ src/_15_generics/_03_generics_interface/IterableFibonacci.java
@@ -0,0 +1,36 @@
+package _15_generics._03_generics_interface;
+
+import java.util.Iterator;
+
+/**
+ * @author lwx
+ * @date 2019/9/1-15:28
+ */
+public class IterableFibonacci extends Fibonacci implements Iterable<Integer> {
+    private int n;
+
+    public IterableFibonacci(int count) {
+        n = count;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return n > 0;
+            }
+
+            @Override
+            public Integer next() {
+                n--;
+                return IterableFibonacci.this.next();
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        for (int i : new IterableFibonacci(18))
+            System.out.print(i + " ");
+    }
+}
