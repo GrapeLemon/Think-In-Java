8403de85857eb4a4e4eaa021599ae1e8186c1f3c
lwx <279214678@qq.com>
2019-09-02 16:02:24 +0800

    讲道理这个客户(队列)-服务员(列表,但随即读取里面的人)模型很有意思啊

---
+++ src/_15_generics/_05_use_in_anonymous_inner_class/BankTeller.java
@@ -0,0 +1,61 @@
+package _15_generics._05_use_in_anonymous_inner_class;
+
+import _15_generics._03_generics_interface.Generator;
+import _15_generics._04_generics_methods._03_generators.Generators;
+
+import java.util.*;
+
+/**
+ * @Author wx-li
+ * @Date 2019/9/2 15:51
+ * A very simple band teller simulation
+ **/
+
+class Customer {
+    private static long counter = 1;
+    private final long id = counter++;
+    private Customer(){}
+    public String toString(){ return "Customer " + id; }
+    //A method to produce Generator objects:
+    
+    public static Generator<Customer> generator() {
+        return new Generator<Customer>() {
+            @Override
+            public Customer next() {
+                return new Customer();
+            }
+        };
+    }
+}
+class Teller {
+    private static long counter = 1;
+    private final long id = counter++;
+    private Teller(){}
+    public String toString(){ return "Teller " + id; }
+    //A method to produce Generator objects:
+    
+    public static Generator<Teller> generator() {
+        return new Generator<Teller>() {
+            @Override
+            public Teller next() {
+                return new Teller();
+            }
+        };
+    }
+}
+
+public class BankTeller {
+    public static void serve(Teller t, Customer c) {
+        System.out.println(t + " serves " + c);
+    }
+
+    public static void main(String[] args) {
+        Random rand = new Random(47);
+        Queue<Customer> line = new LinkedList<>();
+        Generators.fill(line, Customer.generator(), 15);
+        List<Teller> tellers = new ArrayList<>();
+        Generators.fill(tellers, Teller.generator(), 4);
+        for (Customer c : line)
+            serve(tellers.get(rand.nextInt(tellers.size())), c);
+    }
+}
