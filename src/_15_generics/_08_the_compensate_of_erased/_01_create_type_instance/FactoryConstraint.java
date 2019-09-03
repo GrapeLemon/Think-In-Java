62451f7a72935d862cf52e665df5c4b77baa59f7
lwx <279214678@qq.com>
2019-09-03 09:09:14 +0800

    这个例子的手法有点高级了：利用了继承来实现间接利用泛型参数来创建对象

--- src/_15_generics/_08_the_compensate_of_erased/_01_create_type_instance/FactoryConstraint.java
+++ src/_15_generics/_08_the_compensate_of_erased/_01_create_type_instance/FactoryConstraint.java
@@ -1,6 +1,7 @@
 package _15_generics._08_the_compensate_of_erased._01_create_type_instance;
 
 import _14_type_info._04_register_factorymethod.Factory;
+import com.sun.org.apache.bcel.internal.generic.NEW;
 
 /**
  * @Author wx-li
@@ -12,10 +13,29 @@ interface FactoryI<T> {
 
 class Foo2<T> {
     private T x;
-    public <F extends FactoryI<T>> Foo2() {
-        
+    public <F extends FactoryI<T>> Foo2(F factory) {
+        x = factory.create();
     }
 }
 
+class IntegerFactory implements FactoryI<Integer> {
+    @Override
+    public Integer create() {
+        return new Integer(0);
+    }
+}
+
+class Widget {
+    public static class Factory implements FactoryI<Widget> {
+        @Override
+        public Widget create() {
+            return new Widget();
+        }
+    }
+}
 public class FactoryConstraint {
+    public static void main(String[] args) {
+        new Foo2<Integer>(new IntegerFactory());
+        new Foo2<>(new Widget.Factory());
+    }
 }
