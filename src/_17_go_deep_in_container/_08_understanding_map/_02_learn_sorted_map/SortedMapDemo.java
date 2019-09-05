8cd3ae0971d00faaf65b84bc69694323ad886526
lwx <279214678@qq.com>
2019-09-05 17:35:36 +0800

    对treemap的基本使用,有序的数据可以通过API快速的拿到首尾

--- src/_17_go_deep_in_container/_08_understanding_map/_02_learn_sorted_map/SortedMapDemo.java
+++ src/_17_go_deep_in_container/_08_understanding_map/_02_learn_sorted_map/SortedMapDemo.java
@@ -1,5 +1,10 @@
 package _17_go_deep_in_container._08_understanding_map._02_learn_sorted_map;
 
+import _17_go_deep_in_container._08_understanding_map.CountingMapData;
+
+import java.util.Iterator;
+import java.util.TreeMap;
+
 /**
  * @author wx-li
  * @date 2019/9/5-15:52
@@ -7,5 +12,25 @@ package _17_go_deep_in_container._08_understanding_map._02_learn_sorted_map;
  **/
 public class SortedMapDemo {
     public static void main(String[] args) {
+        TreeMap<Integer,String> sortedMap =
+            new TreeMap<>(new CountingMapData(10));
+        System.out.println(sortedMap);
+        Integer low = sortedMap.firstKey();
+        Integer high = sortedMap.lastKey();
+        System.out.println(low);
+        System.out.println(high);
+        Iterator<Integer> it = sortedMap.keySet().iterator();
+        for (int i = 0; i <= 6; i++) {
+            if (i == 3) low = it.next();
+            if (i == 6) high = it.next();
+            else it.next();
+        }
+        System.out.println(low);
+        System.out.println(high);
+        System.out.println(sortedMap.subMap(low,high));
+        // 点进去看发现有个inclusive参数,决定是否包含头部or尾部
+        // 现在的处理是只包含头不包含尾
+        System.out.println(sortedMap.headMap(high));
+        System.out.println(sortedMap.tailMap(low));
     }
 }
