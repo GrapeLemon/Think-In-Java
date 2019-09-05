80425dbd581031e7733f54214c68cdc6846f351d
lwx <279214678@qq.com>
2019-09-05 18:58:17 +0800

    未完待续 map深入探索

---
+++ src/_17_go_deep_in_container/_09_hash_and_hashcode/_01_understanding_hashcode_method/SlowMap.java
@@ -0,0 +1,44 @@
+package _17_go_deep_in_container._09_hash_and_hashcode._01_understanding_hashcode_method;
+
+import java.util.*;
+
+/**
+ * @author wx-li
+ * @date 2019/9/5-18:43
+ * A Map implemented with ArrayLists
+ **/
+public class SlowMap<K,V> extends AbstractMap<K, V> {
+    private List<K> keys = new ArrayList<>();
+    private List<V> values = new ArrayList<>();
+    public V put(K key, V value) {
+        V oldValue = get(key); // The old value or null
+        if (!keys.contains(key)) {
+            keys.add(key);
+            values.add(value);
+        } else {
+            values.set(keys.indexOf(key), value);
+        }
+        return oldValue;
+    }
+    public V get(Object key) {  // key is type Object, not K
+        if (!keys.contains(key)) {
+            return null;
+        }
+        return values.get(keys.indexOf(key));
+    }
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        Set<Map.Entry<K,V>> set = new HashSet<>();
+        Iterator<K> ki = keys.iterator();
+        Iterator<V> vi = values.iterator();
+        while (ki.hasNext()) {
+            set.add(new MapEntry<>(ki.next(), vi.next()));
+        }
+        return set;
+    }
+
+    public static void main(String[] args) {
+        SlowMap<String, String> m = new SlowMap<>();
+        m.putAll();
+    }
+}
