d6aa1dce90b232b5c81832ee5762e69176d2577e
lwx <279214678@qq.com>
2019-09-05 16:16:44 +0800

    工具类加map全家福

---
+++ src/_17_go_deep_in_container/_08_understanding_map/_02_learn_sorted_map/SortedMapDemo.java
@@ -0,0 +1,11 @@
+package _17_go_deep_in_container._08_understanding_map._02_learn_sorted_map;
+
+/**
+ * @author wx-li
+ * @date 2019/9/5-15:52
+ * What you can do with a TreeMap.
+ **/
+public class SortedMapDemo {
+    public static void main(String[] args) {
+    }
+}
