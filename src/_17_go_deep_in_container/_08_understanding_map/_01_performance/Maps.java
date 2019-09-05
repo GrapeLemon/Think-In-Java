14a3ed193ef7319ef90266d6b1bb5067f79e5647
lwx <279214678@qq.com>
2019-09-05 17:25:33 +0800

    补充对map的使用

--- src/_17_go_deep_in_container/_08_understanding_map/_01_performance/Maps.java
+++ src/_17_go_deep_in_container/_08_understanding_map/_01_performance/Maps.java
@@ -1,6 +1,9 @@
 package _17_go_deep_in_container._08_understanding_map._01_performance;
 
-import java.util.Map;
+import _17_go_deep_in_container._08_understanding_map.CountingMapData;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author wx-li
@@ -15,5 +18,35 @@ public class Maps {
     }
     public static void test(Map<Integer,String> map) {
         System.out.println(map.getClass().getSimpleName());
+        map.putAll(new CountingMapData(25));
+        // Map has 'Set' behavior for keys:
+        map.putAll(new CountingMapData(25));
+        printKeys(map);
+        // Producing a Collection of the values:
+        System.out.print("Values: ");
+        System.out.println(map.values());
+        System.out.println(map);
+        System.out.println("map.containsKey(11) = " + map.containsKey(11));
+        System.out.println("map.get(11) = " + map.get(11));
+        System.out.println("map.containsValue(\"F0\") = " + map.containsValue("F0"));
+        Integer key = map.keySet().iterator().next();
+        System.out.println("First key in map: " + key);
+        map.remove(key);
+        printKeys(map);
+        map.clear();
+        System.out.println("map.isEmpty() = " + map.isEmpty());
+        map.putAll(new CountingMapData(25));
+        // Operations on the Set change the Map:
+        map.keySet().removeAll(map.keySet());
+        System.out.println("map.isEmpty(): " + map.isEmpty());
+    }
+
+    public static void main(String[] args) {
+        test(new HashMap<>());
+        test(new TreeMap<>());
+        test(new LinkedHashMap<>());
+        test(new IdentityHashMap<>());
+        test(new ConcurrentHashMap<>());
+        test(new WeakHashMap<>());
     }
 }
