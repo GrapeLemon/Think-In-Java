166848143ff92de98f376a9f72772ec596367c3b
lwx <279214678@qq.com>
2019-09-08 21:42:38 +0800

    通过重写toString方法丰富了我们输出的相关信息

---
+++ src/_19_enum/_02_add_new_methods/SpaceShip.java
@@ -0,0 +1,21 @@
+package _19_enum._02_add_new_methods;
+
+/**
+ * @author lwx
+ * @date 2019/9/8-21:28
+ */
+public enum  SpaceShip {
+    SCOUT, CARGO, TRANSPORT, CRUISER, BATTLESHIP, MOTHERSHIP;
+
+    public String toString() {
+        String id = name();
+        String lower = id.substring(1).toLowerCase();
+        return id.charAt(0) + lower;
+    }
+
+    public static void main(String[] args) {
+        for (SpaceShip s : values()) {
+            System.out.println(s);
+        }
+    }
+}
